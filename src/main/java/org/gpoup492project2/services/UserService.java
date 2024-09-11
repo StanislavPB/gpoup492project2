1afd08d8764dc8b774f6864f01e6028d2175bb9c
Kseniia156 <hedyrover@gmail.com>
2024-09-11 09:53:50 +0200

    добавление комментов

--- src/main/java/org/gpoup492project2/services/UserService.java
+++ src/main/java/org/gpoup492project2/services/UserService.java
@@ -4,6 +4,8 @@ package org.gpoup492project2.services;
 import org.gpoup492project2.Repository.UserRepository;
 import org.gpoup492project2.entity.User;
 
+import java.util.Optional;
+
 public class UserService {
     private UserRepository userRepository;
 
@@ -11,14 +13,25 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void registerUser(String username, String password){
+    public void registerUser(String username, String password) {
         User user = new User(username, password);
         userRepository.addUser(user);
         System.out.println("Пользователь " + username + " зарегистрирован");
     }
 
-    public boolean authenticateUser(String username, String password){
+    public boolean authenticateUser(String username, String password) {
         User user = userRepository.findUser(username);
         return user != null && user.getPassword().equals(password);
     }
+
+    public User createUser(int id, String name, String surname, String login, String password) {
+        User user = new User(id, name, surname, login, password, name);
+        userRepository.save(user);
+        return user;
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.getUserById(id);
+    }
 }
+
